933ab94c7b78684e2ddda6fd3ff9191834e02439
iva2410 <iva24102000@gmail.com>
2026-02-17 20:55:12 +0100

    update

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,30 @@
-U uvodu bih opisala temu i cilj rada,
- zatim poglavlje o web scrapingu (šta je, prednosti i mane, kao i alati).
- Nakon toga bih obradila u posebnim poglavljima analizu sentimenta i
- sisteme za preporuke.
- Na kraju bih opisala sam rad, implementaciju, korišćene alate i logiku sistema, analizu rezultata i zaključak
+import pandas as pd
+from textblob import TextBlob
+
+print("Učitavam očišćene recenzije...")
+df = pd.read_csv('ociscene_recenzije.csv')
+
+def analiziraj_tekst(tekst):
+    tekst = str(tekst)
+    blob = TextBlob(tekst)
+
+    return blob.sentiment.polarity
+
+df['Sentiment_Score'] = df['Recenzija'].apply(analiziraj_tekst)
+
+def kategorija(skor):
+    if skor > 0.1:
+        return 'Pozitivna'
+    elif skor < -0.1:
+        return 'Negativna'
+    else:
+        return 'Neutralna'
+
+df['Emocija'] = df['Sentiment_Score'].apply(kategorija)
+
+print("\n--- PROSEČAN SENTIMENT PO GRADOVIMA ---")
+statistika = df.groupby('Grad')['Sentiment_Score'].mean()
+print(statistika)
+
+df.to_csv('finalna_analiza.csv', index=False, encoding='utf-8')
+print("\nSve je završeno! Fajl 'finalna_analiza.csv' je spreman.")
